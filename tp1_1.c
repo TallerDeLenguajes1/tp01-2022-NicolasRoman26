0db83e1648373ffb136d62489d36e75a6f128e75
Nicolas Roman <nick.26blue@gmail.com>
2022-03-22 12:44:05 -0300

    puntos e,f,h,i,j completados

--- tp1_1.c
+++ tp1_1.c
@@ -2,6 +2,17 @@
 
 void main(void)
 {
+    int numero = 26;
+    int *puntNumero;
+
+    puntNumero = &numero;
+
     printf("hola mundo\n");
+    printf("Contenido del puntero: %d \n", *puntNumero);
+    printf("Direccion de memoria almacenada por el puntero: %u \n", puntNumero);
+    printf("Direccion de memoria de la variable: %u \n", &numero);
+    printf("Direccion de memoria del puntero: %u \n", &puntNumero);
+    printf("Tamaño de memoria utilizado por la variable: %lu \n", sizeof(numero));
+
     return;
 }
